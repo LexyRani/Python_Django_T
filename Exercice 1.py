1e1ecd346549e20befb3947d5b3b72c163338492
aceraline <alix.ceraline@gmail.com>
2026-03-29 20:01:26 +0200

    Add Exo1

---
+++ Exercice 1.py
@@ -0,0 +1,32 @@
+largerMax = 100
+
+def add_space(bloc):
+    for tup in bloc.values():
+        lenStr = min(len(tup[0]), largerMax - 2)
+        if tup[1] == True:
+            print(f"|{' ' * (largerMax - lenStr - 2)}{tup[0][:lenStr].lower()}|")
+
+def main():
+
+
+    bloc1 = {1: ("Le code propre facilite la maintenance", True)}
+    bloc2 = {1: ("Tester souvent évite beaucoup d erreurs", True),
+             2: ("Cette phrase ne doit pas s afficher", False)}
+    bloc3 = {1: ("Cette phrase ne doit pas s afficher", True),
+            2: ("Un bon code doit rester simple et clair", False),
+            3: ("La simplicité améliore la qualité du code", True),
+            4: ("Refactoriser améliore la compréhension", True)}
+    dico = [bloc1,
+            bloc2,
+            bloc3]
+
+    for element in dico:
+        print("-" * largerMax)
+        add_space(element)
+        print("-" * largerMax)
+
+if __name__ == "__main__":
+    if largerMax < 2:
+        print("Error: Your largerMax has to be more than 2")
+        exit()
+    main()
