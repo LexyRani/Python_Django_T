8bb196ad4866d360a9a7ce98456f605d1f6dd38f
aceraline <alix.ceraline@gmail.com>
2026-04-05 19:24:55 +0200

    Add a new way to see the format of the expiration_date

--- Exercice 2/WebAppDjango/shop/views.py
+++ Exercice 2/WebAppDjango/shop/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 from .models import Product
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from.form import ProductForm
+from django.views.generic.edit.UpdateView import UpdateView
 
 class ProductListView(ListView):
     model = Product
@@ -12,9 +14,11 @@ class ProductListView(ListView):
 
 class ProductCreateView(CreateView):
     model = Product
+    form_class = ProductForm
     template_name = 'shop/product_form.html'
-    fields = ['name', 'price', 'expiration_date']
     success_url = reverse_lazy('product-list')
+
+
 
 
    
