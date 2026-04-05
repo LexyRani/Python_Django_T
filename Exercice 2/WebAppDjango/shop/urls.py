896b186b5b9025611f9af5c98c40460d8ea78273
aceraline <alix.ceraline@gmail.com>
2026-04-05 18:54:29 +0200

    Create ProductCreateView and the url create with the template to create a product

--- Exercice 2/WebAppDjango/shop/urls.py
+++ Exercice 2/WebAppDjango/shop/urls.py
@@ -1,6 +1,8 @@
 from .views import ProductListView
+from .views import ProductCreateView
 from django.urls import path
 
 urlpatterns = [
-    path("products/",ProductListView.as_view(), name="product-list")
+    path("products/",ProductListView.as_view(), name="product-list"),
+    path("create/", ProductCreateView.as_view(), name="product-create"),
 ]
--- Exercice 2/WebAppDjango/shop/views.py
+++ Exercice 2/WebAppDjango/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class ProductListView(ListView):
     model = Product
@@ -8,5 +10,11 @@ class ProductListView(ListView):
     context_object_name = 'products'
     paginate_by = 5
 
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'shop/product_form.html'
+    fields = ['name', 'price', 'expiration_date']
+    success_url = reverse_lazy('product-list')
 
 
+   
