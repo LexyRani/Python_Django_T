232b6a95012b3661d8ef611cd1d8b7eb54afc59d
aceraline <alix.ceraline@gmail.com>
2026-04-06 05:02:31 +0200

    add invoiceForm and invoiceItemFormSet

--- Exercice 2/WebAppDjango/shop/form.py
+++ Exercice 2/WebAppDjango/shop/form.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Product
+from django.forms import inlineformset_factory
+from .models import Product, Invoice, InvoiceItem
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -8,3 +10,27 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'expiration_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+
+class InvoiceForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ['created_at']
+        widgets = {
+            'created_at': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class InvoiceItemForm(forms.ModelForm):
+    class Meta:
+        model = InvoiceItem
+        fields = ['product', 'quantity', 'unit_price']
+
+
+InvoiceItemFormSet = inlineformset_factory(
+    Invoice,
+    InvoiceItem,
+    form=InvoiceItemForm,
+    extra=3,
+    can_delete=True
+)
--- Exercice 2/WebAppDjango/shop/models.py
+++ Exercice 2/WebAppDjango/shop/models.py
@@ -1,19 +1,37 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Product(models.Model):
-
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     expiration_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Invoice(models.Model):
-    created_at=models.DateField()
-    
+    created_at = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"Facture #{self.id}"
+
+    def total_products(self):
+        return sum(item.quantity for item in self.invoiceitem_set.all())
+
+    def total_price(self):
+        return sum(item.line_total() for item in self.invoiceitem_set.all())
+
 
 class InvoiceItem(models.Model):
-
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    unit_price= models.DecimalField(max_digits=8, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def line_total(self):
+        return self.quantity * self.unit_price
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
