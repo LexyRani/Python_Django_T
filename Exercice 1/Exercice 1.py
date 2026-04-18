b31e655a89b9ef554f1874070e78f33273b3cc83
aceraline <alix.ceraline@gmail.com>
2026-04-18 13:29:11 +0200

    replace separate bloc variable with a single dico structure

--- Exercice 1/Exercice 1.py
+++ Exercice 1/Exercice 1.py
@@ -7,18 +7,17 @@ def add_space(bloc):
             print(f"|{' ' * (MAX_WIDTH - lenStr - 2)}{tup[0][:lenStr].lower()}|")
 
 def main():
-    bloc1 = {1: ("Le code propre facilite la maintenance", True)}
-    bloc2 = {1: ("Tester souvent évite beaucoup d erreurs", True),
-             2: ("Cette phrase ne doit pas s afficher", False)}
-    bloc3 = {1: ("Cette phrase ne doit pas s afficher", True),
+    dico = {
+        "bloc1":{1: ("Le code propre facilite la maintenance", True)},
+        "bloc2":{1: ("Tester souvent évite beaucoup d erreurs", True),
+             2: ("Cette phrase ne doit pas s afficher", False)},
+        "bloc3": {1: ("Cette phrase ne doit pas s afficher", True),
             2: ("Un bon code doit rester simple et clair", False),
             3: ("La simplicité améliore la qualité du code", True),
             4: ("Refactoriser améliore la compréhension", True)}
-    dico = [bloc1,
-            bloc2,
-            bloc3]
+    }
 
-    for element in dico:
+    for element in dico.values():
         print("-" * MAX_WIDTH)
         add_space(element)
         print("-" * MAX_WIDTH)
